3db2a973bc36ff7c4bd4e4bde33747f466260b2f
German Lopez <german@rootstrap.com>
2018-05-25 10:27:30 -0300

    Initial commit

---
+++ Unity/Assets/TestScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TestScript : MonoBehaviour {
+
+    public Renderer renderer;
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			setAnimation(!animator.enabled);
+		}
+	}
+
+	public void changeColor(string hexString) {
+		Color c = new Color();
+     	ColorUtility.TryParseHtmlString(hexString, out c);
+     	if (GetComponent<Renderer>() != null) {
+            renderer.material.color = c;
+     		System.Console.WriteLine("Changed color to: " + hexString);
+     	}
+	}
+
+	public void stopAnimating() {
+		setAnimation(false);
+	}
+
+	[DllImport("__Internal")]
+	static extern void appAnimatingModel(bool active);
+	
+	public void setAnimation(bool active) {
+		animator.enabled = active;
+		#if UNITY_IOS
+			appAnimatingModel(active);
+		#endif
+	}
+}
